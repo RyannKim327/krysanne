1ff3a6783e9dae9ad2e51c3899a969fadfe45b61
RyannKim327 <rksesgundo123@gmail.com>
2026-07-12 19:56:55 +0800

    07-12-26 19:56 [feat: deletion of message for delete thread]

--- src/script/clear-chat.ts
+++ src/script/clear-chat.ts
@@ -2,7 +2,7 @@ import { aiResponse } from "@/interface";
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import TelegramBot, { Message } from "node-telegram-bot-api";
 
-export default function clearChat(api: TelegramBot, event: Message, body: aiResponse) {
+export default async function clearChat(api: TelegramBot, event: Message, body: aiResponse) {
 
   let user = event.from?.id.toString() || event.chat.id.toString()
 
@@ -24,5 +24,10 @@ export default function clearChat(api: TelegramBot, event: Message, body: aiResp
 
   writeFileSync("data/dataset.json", JSON.stringify(store, null, 2), "utf-8")
   api.deleteForumTopic(event.chat.id, event.message_thread_id as number)
-  api.sendMessage(event.chat.id, body.message)
+
+  const message = await api.sendMessage(event.chat.id, `The thread ${event.reply_to_message?.forum_topic_created?.name} is now deleted`)
+
+  setTimeout(() => {
+    api.deleteMessage(message.chat.id, message.message_id)
+  }, 5000)
 }
